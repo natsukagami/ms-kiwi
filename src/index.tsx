edef743cc2330818399efebbe6b1db80ea7d42e4
Trung Nguyen <ntrung03@outlook.com>
2020-07-29 17:33:40 +0700

    HOST shouldn't be hardcoded

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 // Must be the first import
 import "preact/debug";
 
-const HOST = "http://localhost:8080";
+const HOST = window.location.origin;
 
 render(<App host={HOST} />, document.getElementById("main") as HTMLElement);
